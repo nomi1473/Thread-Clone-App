1ae633eedc66b8c780b80158955f16051994df59
admin <nomi18401840@gmail.com>
2024-07-10 10:13:42 +0500

    Initial Commit

---
+++ thread clone/thread_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  thread_cloneApp.swift
+//  thread clone
+//
+//  Created by Admin on 10/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct thread_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
